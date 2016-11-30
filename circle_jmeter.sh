8e41d1a4e3c91971f0fcd613f61a5a69c420d103
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-11-30 16:01:47 -0500

    test results cat

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -4,3 +4,4 @@
     cd bin
     sh jmeter.sh -v
     sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.csv" -Djmeter.sh.save.saveservice.output_format=csv -Jenv=QA
+    sh cat ../projects/"ABCD.csv"
