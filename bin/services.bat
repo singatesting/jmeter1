e7b80d854023343dff903ca3e5af0edab2df6e6e
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-11-30 11:40:22 -0500

    first commit

---
+++ bin/services.bat
@@ -0,0 +1,30 @@
+call jmeter -n -t ../projects/"Annotations Favorites and Notes API.jmx" -l ../projects/results/"Annotations Favorites and Notes API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Asset API.jmx" -l ../projects/results/"Asset API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Assignment API.jmx" -l ../projects/results/"Assignment API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Class API.jmx" -l ../projects/results/"Class API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"CMR_Health Check.jmx" -l ../projects/results/"CMR_Health Check.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"ContentArea.jmx" -l ../projects/results/"Content Area API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Context API.jmx" -l ../projects/results/"Context API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Course API.jmx" -l ../projects/results/"Course API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Discipline API.jmx" -l ../projects/results/"Discipline API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Education Entity API.jmx" -l ../projects/results/"Education Entity API (District).xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Evaluation API.jmx" -l ../projects/results/"Evaluation API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Global Search API.jmx" -l ../projects/results/"Global Search API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Group API.jmx" -l ../projects/results/"Group API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"iAtlas Asset API.jmx" -l ../projects/results/"iAtlas Asset API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"ISBN API.jmx" -l ../projects/results/"ISBN API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Planner_General API.jmx" -l ../projects/results/"Planner_General API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Planner_Custom API.jmx" -l ../projects/results/"Planner_Custom API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Planner_US Holidays API.jmx" -l ../projects/results/"Planner_US Holidays API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Product API.jmx" -l ../projects/results/"Product API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Program API.jmx" -l ../projects/results/"Program API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Resource API.jmx" -l ../projects/results/"Resource API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Scorm Cloud Test Plan.jmx" -l ../projects/results/"Scorm Cloud Test Plan.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"SEG_Health Check.jmx" -l ../projects/results/"SEG_Health Check.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Standard API.jmx" -l ../projects/results/"Standard API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Tag API.jmx" -l ../projects/results/"Tag API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Tree API.jmx" -l ../projects/results/"Tree API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"Tree API - New Design.jmx" -l ../projects/results/"Tree API - New Design.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"User API.jmx" -l ../projects/results/"User API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+call jmeter -n -t ../projects/"XAPI.jmx" -l ../projects/results/XAPI.xml -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+
---
+++ bin/services_SmokeTest.bat
@@ -0,0 +1,3 @@
+call jmeter -n -t ../projects/"2 Smoke Test Driver Script API.jmx" -l ../projects/results/"Smoke Test Driver Script API.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=%1
+
+
