fd34261fa56035196b8d15601c608ac205109c13
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-11-30 15:48:50 -0500

    test results

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -3,4 +3,4 @@
     rm -rf projects/results/*.csv
     cd bin
     sh jmeter.sh -v
-    sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/results/"Asset API.csv" -Djmeter.sh.save.saveservice.output_format=csv -Jenv=QA
+    sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.csv" -Djmeter.sh.save.saveservice.output_format=csv -Jenv=QA
