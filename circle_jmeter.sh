5d2c24cdff11a8f44bf7434d363c584e1699fede
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-11-30 15:33:17 -0500

    free api testing 3

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -3,4 +3,4 @@
     rm -rf projects/results/*.csv
     cd bin
     sh jmeter.sh -v
-    sh jmeter.sh -n -t ../projects/"Free_API.jmx" -l ../projects/results/"Asset API.csv" -Djmeter.sh.save.saveservice.output_format=csv -Jenv=QA
+    sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/results/"Asset API.csv" -Djmeter.sh.save.saveservice.output_format=csv -Jenv=QA
