cd882922ee628a6a36db33e53309f63a652a41d1
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-12-01 11:36:31 -0500

    extra sh file to check9

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -1,8 +1,13 @@
-  rm -rf projects/results/*.jtl
-    rm -rf projects/results/*.xml
-    rm -rf projects/results/*.csv
+
     cd bin
     sh jmeter.sh -v
     sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=QA
     cat ../projects/"ABCD.xml"
-    sh test_result.sh
+    emails=$(grep -oP '(?<=failure>)[^<]+' "../projects/ABCD.xml")
+    echo ${emails[0]}
+
+     if [ ${emails[0]} = true ]; then
+            ex=1
+        fi
+    echo $ex
+    sleep 5
