569043b8ef25781ebff06ae49543e15b089a1bae
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-12-01 10:30:37 -0500

    extra sh file to check

---
+++ test_result.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+emails=($(grep -oP '(?<=failure>)[^<]+' "../projects/"ABCD.xml"))
+
+for i in ${!emails[*]}
+do
+  echo "$i" "${emails[$i]}"
+  # instead of echo use the values to send emails, etc
+done
