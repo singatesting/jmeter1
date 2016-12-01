50356ddd4174fb3e3bbe589f08ab84e9712684bf
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-12-01 13:04:16 -0500

    extra sh file to check92

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -3,6 +3,7 @@
     sh jmeter.sh -v
     sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=QA
     cat ../projects/ABCD.xml
+
+
     cat ../projects/ABCD.xml |
-    getElementViaXPath --path='/testResults/httpSample/assertionResult' |
-    sed -e 's%(^<title>|</title>$)%%g' > titleOfXHTMLPage.txt
+    grep -oPm1 "(?<=<failure>)[^<]+"
