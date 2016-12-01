9d7d1168ac7f4b9119a57f53eb39b51f7ef6c10e
Kusampudi <singaraju.kusampudi@mheducation.com>
2016-12-01 13:09:23 -0500

    extra sh file to check93

--- circle_jmeter.sh
+++ circle_jmeter.sh
@@ -1,9 +1,7 @@
-
-    cd bin
-    sh jmeter.sh -v
-    sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=QA
-    cat ../projects/ABCD.xml
-
-
-    cat ../projects/ABCD.xml |
-    grep -oPm1 "(?<=<failure>)[^<]+"
+cd bin
+sh jmeter.sh -v
+sh jmeter.sh -n -t ../projects/"ABCD.jmx" -l ../projects/"ABCD.xml" -Djmeter.save.saveservice.output_format=xml -Jenv=QA
+cat ../projects/ABCD.xml
+cat ../projects/ABCD.xml | grep -oPm1 "(?<=<failure>)[^<]+"
+str=cat ../projects/ABCD.xml | grep -oPm1 "(?<=<failure>)[^<]+"
+echo $str
